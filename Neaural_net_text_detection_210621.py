048e13f25d85f52417ccafff86a4f6973c40094f
JackDCornwall <contact@jackcornwall.co.uk>
2021-06-22 17:37:39 +0100

    Mode run and sucessful output implemented, however GPU is not being fully utilized

--- Neaural_net_text_detection_210621.py
+++ Neaural_net_text_detection_210621.py
@@ -9,10 +9,17 @@
 #Description: Running through this tutorial: https://www.youtube.com/watch?v=y1ZrOs9s2QA
 #Images can be found here: https://www.kaggle.com/scolianni/mnistasjpg or http://yann.lecun.com/exdb/mnist/ in raw form
 '''
+#importing require packages
 import numpy as np
 import cv2
 import os
 from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.layers import Dropout,Flatten,Dense
+from keras.layers.convolutional import Conv2D, MaxPooling2D
 
 #####################----Settings----#####################
 path = "Training Data/trainingSet/TesttrainingSet" #for quicker testing (25 images per file)
@@ -20,6 +27,24 @@ path = "Training Data/trainingSet/TesttrainingSet" #for quicker testing (25 imag
 test_ratio = 0.2 #test fraction
 valid_ratio = 0.05 #validation fraction
 ##########################################################
+
+#defining function to preprocess images
+def preProcess(img):
+
+    #converting to greyscale
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    #equalising image (distribute the lighting evenly)
+    img = cv2.equalizeHist(img)
+
+    return img #outputting processed image
+
+# #viewing an example pre processed image
+# test_img = cv2.imread(path+"/0/img_4.jpg") #importing test imagemg
+# test_img = cv2.resize(test_img,(200,200))#resizing image so it is usable
+# cv2.imshow("Image before processing",test_img) #displaying unprocessed image
+# cv2.imshow("Preprocessed image",preProcess(test_img)) #displaying processed image
+# cv2.waitKey(0)
 
 #listing all available directories
 dir_list = os.listdir(path)
@@ -33,9 +58,9 @@ img_cat_list = [] #image value will be store here (correct answer)
 #the index for img_list and img_car_list will correspond to each other, however a DF could be used for this
 
 #printing system update
-print("Total folders detected:",num_class) #class counnt
+print("Total folders detected:",num_class) #class count
 print(" ") #formatting nicely
-print("Currently importing folder number:") #header
+print("Currently importing & pre-processing folder number:") #header
 
 #looping through each folder
 for dir in dir_list:
@@ -53,6 +78,13 @@ for dir in dir_list:
         #reading in current image array with OpenCV
         img_array = cv2.imread(img_path)
 
+        #inverting colors of image
+        img_array = 255-img_array
+        #this is done because they are being read as white letters on a black background and it should be the other way around
+
+        #pre-processing image array as they are looped through
+        img_array = preProcess(img_array)
+
         #appending data
         img_array_list.append(img_array) #appending entire BGR array as list item
         img_cat_list.append(int(dir)) #appening digit number as int, folder names must be numbers (correct answer)
@@ -65,9 +97,14 @@ print(" ") #for formatting
 img_array_list = np.array(img_array_list)
 img_cat_list = np.array(img_cat_list)
 
+#adding depth
+array_shape = img_array_list.shape #extracting shape
+img_array_list = img_array_list.reshape(array_shape[0],array_shape[1],array_shape[2],1) #reshaping array (adding depth)
+#the neural network requires the extra dimension
+
 #checking we have arrays of the desired shape
-#print(img_array_list.shape)
-#print(img_cat_list.shape)
+print(img_array_list.shape)
+print(img_cat_list.shape)
 
 ###Splitting data into train/test/validation
 #splitting all data into train and test
@@ -81,12 +118,97 @@ print("Training array size:",img_train.shape)
 print("Test array size:",img_test.shape)
 print("Validation array size:",img_valid.shape)
 
+#list to store training category names
+categories = []
+
 #running through each number and ensuring there is sufficient examples of it
 for dir in dir_list:
 
     #printing system message to enesure we are happy with number of training images per digit
     print("For folder",dir,"there are:",len(np.where(cat_train == int(dir))[0]),"training images")
     #this again will only work if folders are digits
+
+    #storing individual category names
+    categories.append(dir)
+
+#calculating number of categories
+cat_len = len(categories)
+
+#function to skew images so training becomes moreo general
+dataGen = ImageDataGenerator(
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    zoom_range=0.2,
+    shear_range=0.1,
+    rotation_range=10 #degrees
+)
+
+#performing "skewing"
+dataGen.fit(img_train)
+
+#converting folder dirs to training categories
+cat_train = to_categorical(cat_train,cat_len)
+cat_test = to_categorical(cat_test,cat_len)
+cat_valid = to_categorical(cat_valid,cat_len)
+
+#defining training model using LeNet Convolutional Neural Network
+def LeNet_Model():
+
+    #these parameters will be required to train model
+    noOfFilters = 60
+    sizeOfFilter1 = (5,5)
+    sizeOfFilter2 = (3,3)
+    sizeOfPool = (2,2)
+    noOfNodes = 500
+
+    model = Sequential()
+
+    #adding first convolutional layer
+    model.add((Conv2D(noOfFilters,
+                      sizeOfFilter1,
+                      input_shape = (28,28,1),
+                      activation = "relu"
+                      )))
+
+    #adding second convolutional layer (no need to specify dimensions/shape again
+    model.add((Conv2D(noOfFilters,sizeOfFilter1,input_shape = (28,28,1),activation = "relu")))
+
+    #adding pooling layer
+    model.add(MaxPooling2D(pool_size=sizeOfPool))
+
+    #adding third convolutional layer (with half the number of filters and smaller filters)
+    model.add((Conv2D(noOfFilters//2,sizeOfFilter2,input_shape = (28,28,1),activation = "relu")))
+
+    #adding 4th convolutional layer
+    model.add((Conv2D(noOfFilters//2,sizeOfFilter2,input_shape = (28,28,1),activation = "relu")))
+
+    #adding another pooling layer
+    model.add(MaxPooling2D(pool_size=sizeOfPool))
+
+    #adding dropout layer (these help reduce overfitting making the model more generic)
+    model.add(Dropout(0.5))
+
+    #Adding flaten layer
+    model.add(Flatten())
+
+    #Adding dense layer
+    model.add(Dense(noOfNodes,activation="relu"))
+
+    #adding dropout layer
+    model.add(Dropout(0.5))
+
+    #Adding final dense layer with the number of classes and softmax activation
+    model.add(Dense(num_class,activation="softmax"))
+
+    #compiling  with Adam optimizer (lr = learning rate
+    model.compile(Adam(learning_rate=0.0001),loss="categorical_crossentropy",metrics=["accuracy"])
+
+    #returning model
+    return model
+
+#creating model
+model = LeNet_Model()
+print(model.summary())
 
 #end of code
 print("Code has run successfully")
